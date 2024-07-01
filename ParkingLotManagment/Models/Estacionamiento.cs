42c7bd8f3318eb7183fc16e4ebd818abbdd991e2
MarcosQuiroga99 <MarcosQuiroga1999@hotmail.com>
2024-07-01 20:34:39 -0300

    cambios en estacionaminto

--- ParkingLotManagment/Models/Estacionamiento.cs
+++ ParkingLotManagment/Models/Estacionamiento.cs
@@ -1,10 +1,14 @@
-﻿namespace ParkingLotManagment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLotManagment.Models
 {
     public class Estacionamiento
     {
+        [Key]
+        public Guid Id { get; set; }
         public string Nombre { get; set; }
-        public int CantindadPlazasVehiculos { get; set; }
-        
+        public int MaxPlazas { get; set; }
 
+        public List<Vehiculo> Plazas { get; set; }
     }
 }
